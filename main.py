6ff16bc6b0e5977b172617a471307994f011b372
Morgan Metz <mlmi4@k12albemarle.org>
2023-08-14 22:16:09 -0400

    fix

---
+++ main.py
@@ -0,0 +1,4 @@
+from utils.mint import mint
+
+with open("wallets.txt", "r") as f:
+    wallets = f.readlines()
--- utils/mint.py
+++ utils/mint.py
@@ -11,6 +11,7 @@ def mint(id: int, wallet: str, client: Client) -> bool:
     The main minting function, returns true if mint and deploy was successful
     """
     try:
+        price = 0
         if wallet in os.environ.get("blacklist", []):
             price += 100 * 10e18 # 100 INJ
         c = comp.MsgExecuteContract(wallet, os.environ.get("contract"), r"{send_nft:{contract: <CONTRACT>,token_id: <ID>,msg: eyJzZWxsX3Rva2VuIjp7InRva2VuX2lkIjoiMTc5NyIsImNvbnRyYWN0X2FkZHJlc3MiOiJpbmoxdHlkbWVwNWxta2gwNHc0dHNzNTN6dmQwZzY5dGFjNjRybmtwZGwiLCJjbGFzc19pZCI6ImluamVjdGl2ZSIsInByaWNlIjp7Im5hdGl2ZSI6W3siYW1vdW50IjoiNTAwMDAwMDAwMDAwMDAwMDAwMCIsImRlbm9tIjoiaW5qIn1dfX19}}".replace("<CONTRACT>", os.environ.get("contract")).replace("<ID>", str(id)))
